8b617ad745a229eddad22eb6eda6c4fa81be7de4
ganeshhp <ganesh.palnitkar@yahoo.co.in>
2022-05-11 16:27:03 +0530

    Update minikube-install-centos.sh

--- minikube-install-centos.sh
+++ minikube-install-centos.sh
@@ -35,7 +35,7 @@ sudo yum install docker-ce docker-ce-cli containerd.io
 
 sudo dnf install curl conntrack -y
 
-sudo su
+# sudo su
 
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/bin/
 
